ad7be2ea9f86cb8c9f9e5d748c77ac0a450b3441
vagrant <vagrant@homestead>
2021-04-29 01:20:13 +0000

    add migration and seeder

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Country extends Model
 {
-    protected $table = 'country';
+    protected $table = 'countries';
     protected $primaryKey = 'id';
 }
--- database/migrations/2021_04_28_182057_create_countries.php
+++ database/migrations/2021_04_28_182057_create_countries.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateGames extends Migration
+class CreateCountries extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CreateGames extends Migration
      */
     public function up()
     {
-        Schema::create('games', function (Blueprint $table) {
+        Schema::create('countries', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100);
-            $table->text('quiz');
-            $table->integer('score');
-            $table->timestamps();
         });
     }
 
@@ -29,6 +26,6 @@ class CreateGames extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('games');
+        Schema::dropIfExists('countries');
     }
 }
